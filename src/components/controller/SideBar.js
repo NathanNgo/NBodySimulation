0f672574989f224c3f57e2bc5e554016ce3c5ff7
Nathan Ngo <nathanj238@gmail.com>
2020-10-13 07:10:02 +1100

    Added child components to InformationPanel component

--- src/components/controller/SideBar.js
+++ src/components/controller/SideBar.js
@@ -4,8 +4,11 @@ import { ControlPanel } from './ControlPanel';
 
 function SideBar() {
     return (
-      <div className="containerSideBar">
-          <InformationPanel />
+      <div className='containerSideBar'>
+          <InformationPanel>
+              <h2 className='subTitle'> Information Panel </h2>
+              <p> The <b>N Body Simulation</b> is degined to represent collisions and interactions between <span className='acc'>N</span> objects. </p>
+          </InformationPanel>
           <ControlPanel />
       </div>
     );
