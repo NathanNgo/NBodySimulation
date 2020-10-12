606951e5cc9be86942075615df7ec21254436827
Nathan Ngo <nathanj238@gmail.com>
2020-10-13 07:10:19 +1100

    Style fix

--- src/components/view/ViewScreen.js
+++ src/components/view/ViewScreen.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 function ViewScreen() {
     return (
-      <div className="containerViewScreen">
-          <h1> View Screen </h1>
+      <div className='containerViewScreen'>
+          <h1 className='test'> View Screen </h1>
       </div>
     );
 }
