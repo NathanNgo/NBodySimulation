98b2bca65da6cd9ff5e7f23afb2ffec24e8848e7
Nathan Ngo <nathanj238@gmail.com>
2020-10-14 19:44:59 +1100

    Refactored components and added Start Button

--- src/components/controller/SideBar.js
+++ src/components/controller/SideBar.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import { InformationPanel } from './InformationPanel';
-import { ControlPanel } from './ControlPanel';
+import { Panel } from './Panel';
+import { Button } from './input/Button';
 
 function SideBar() {
     return (
-      <div className='containerSideBar'>
-          <InformationPanel>
+      <div >
+          <Panel>
               <h2 className='subTitle'> Information Panel </h2>
               <p> The <b>N Body Simulation</b> is degined to represent collisions and interactions between <span className='acc'>N</span> objects. </p>
-          </InformationPanel>
-          <ControlPanel />
+          </Panel>
+          <Panel>
+              <h2 className='subTitle'> Controls </h2>
+              <Button className='startButton'/>
+          </Panel>
       </div>
     );
 }
