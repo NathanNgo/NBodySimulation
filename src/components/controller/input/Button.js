fe621bbf9944a10d7133dd59d4efa804ec08a502
Nathan Ngo <nathanj238@gmail.com>
2020-10-15 16:32:13 +1100

    Reworked Button.js component to be more modular

--- src/components/controller/input/Button.js
+++ src/components/controller/input/Button.js
@@ -4,18 +4,23 @@ import PropTypes from 'prop-types';
 function Button(props) {
     return (
         <button
-            className={props.className}
+            className={`button ${props.className}`}
             type='button'
             onClick={props.onClick}
         >
-            Start
+            <div className='buttonValues'>
+                <img src ={props.iconURL} />
+                { props.children }
+            </div>
         </button>
     );
 }
 
 Button.propTypes = {
+    children: PropTypes.node,
     onClick: PropTypes.func.isRequired,
     className: PropTypes.string.isRequired,
+    iconURL: PropTypes.string
 }
 
 export { Button };
