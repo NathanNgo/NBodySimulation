55d952322a302c5c736575ac66406da2042d9742
Nathan Ngo <nathanj238@gmail.com>
2020-10-15 16:29:03 +1100

    Added control Buttons and included their icons

--- src/components/controller/SideBar.js
+++ src/components/controller/SideBar.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Panel } from './Panel';
 import { Button } from './input/Button';
+import playIcon from '../../css/icons/play_arrow-24px.svg';
+import pauseIcon from '../../css/icons/pause-24px.svg';
+import resetIcon from '../../css/icons/reset-24px.svg';
 
 function SideBar() {
     return (
@@ -11,7 +14,18 @@ function SideBar() {
           </Panel>
           <Panel>
               <h2 className='subTitle'> Controls </h2>
-              <Button className='startButton'/>
+              <Button
+                  className='playButton'
+                  iconURL={playIcon}
+              />
+              <Button
+                  className='pauseButton'
+                  iconURL={pauseIcon}
+              />
+              <Button
+                  className='resetButton'
+                  iconURL={resetIcon}
+              />
           </Panel>
       </div>
     );
