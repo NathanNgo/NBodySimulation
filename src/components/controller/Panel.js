6561532acceb289f94341fe803c0bc36618b75ec
Nathan Ngo <nathanj238@gmail.com>
2020-10-16 17:37:20 +1100

    Reworked Panel component to be closable

--- src/components/controller/Panel.js
+++ src/components/controller/Panel.js
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { Button } from './input/Button';
+import expandMore from '../../css/icons/expand_more-24px.svg';
+import expandLess from '../../css/icons/expand_less-24px.svg';
 
 function Panel(props) {
+    const [ isOpen, setIsOpen ] = useState(true);
+
+    if (isOpen) {
+        return (
+            <div className='containerPanel'>
+                <div className='panelHeader'>
+                    <h2 className='subTitle'> { props.title } </h2>
+                    <Button
+                        className='chevronButton'
+                        iconURL={ isOpen ? expandLess : expandMore }
+                        onClick={ () => setIsOpen(!isOpen) }
+                    />
+                </div>
+                { props.children }
+            </div>
+        );
+    }
+
     return (
-      <div className='containerPanel'>
-          { props.children }
-      </div>
+        <div className='containerPanel panelHeader'>
+            <h2 className='subTitle'> { props.title } </h2>
+            <Button
+                className='chevronButton'
+                iconURL={ isOpen ? expandLess : expandMore }
+                onClick={ () => setIsOpen(!isOpen) }
+            />
+        </div>
     );
 }
 
 Panel.propTypes = {
-    children: PropTypes.node.isRequired
+    children: PropTypes.node.isRequired,
+    title: PropTypes.string.isRequired
 }
 
 export { Panel };
