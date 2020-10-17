143f756cc210f1fa3ea1d08decc336af166be2fc
Nathan Ngo <nathanj238@gmail.com>
2020-10-17 16:52:50 +1100

    Refactored Panel components

--- src/components/controller/SideBar.js
+++ src/components/controller/SideBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Panel } from './Panel';
+import { ClosablePanel } from './ClosablePanel';
 import { Button } from './input/Button';
 import playIcon from '../../css/icons/play_arrow-24px.svg';
 import pauseIcon from '../../css/icons/pause-24px.svg';
@@ -8,10 +8,10 @@ import resetIcon from '../../css/icons/reset-24px.svg';
 function SideBar() {
     return (
       <div>
-          <Panel title='Information Panel'>
-              <p> The <b>N Body Simulation</b> is degined to represent collisions and interactions between <span className='acc'>N</span> objects. </p>
-          </Panel>
-          <Panel title='Controls'>
+          <ClosablePanel title='Information'>
+              <p> The <b>N Body Simulation</b> is designed to represent collisions and interactions between <span className='acc'>N</span> objects. </p>
+          </ClosablePanel>
+          <ClosablePanel title='Controls'>
               <Button
                   className='playButton'
                   iconURL={playIcon}
@@ -27,7 +27,10 @@ function SideBar() {
                   iconURL={resetIcon}
                   //onClick=...
               />
-          </Panel>
+          </ClosablePanel>
+          <ClosablePanel title='Options'>
+              <p> Options go here </p>
+          </ClosablePanel>
       </div>
     );
 }
