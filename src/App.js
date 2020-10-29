2d593b16b0e83626c7c093630811f20e7f8e7691
Nathan Ngo <nathanj238@gmail.com>
2020-10-29 22:58:59 +1100

    Removed redundant comment

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import { NBodySimulation } from './components/NBodySimulation';
 // which might be used by child components. Can be passed via props.
 // e.g. Axios object to communicate to back end (Setting baseURL and other settings).
 function App() {
-    //document.body.style = 'background: #f5f5f4';
     return (
         <NBodySimulation />
     );
