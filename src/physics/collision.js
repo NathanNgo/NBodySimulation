f7d42ef1858e35892b934e42836a9ddfecb02a50
Nathan Ngo <nathanj238@gmail.com>
2020-11-04 01:03:40 +1100

    Finished collisions for circles. Refactored getOrth

--- src/physics/collision.js
+++ src/physics/collision.js
@@ -1,26 +1,59 @@
 import { Circle, Polygon } from './rigid';
-import { vec2 } from './gl-matrix';
+import vec2 from 'gl-matrix/vec2';
 
 function isColliding(obj1, obj2) {
-    const axes1 = obj1 instanceof Circle ? null : getOrth(obj1);
-    const axes2 = obj2 instanceof Circle ? null : getOrth(obj2);
-    
-    // Account for polygon-circle collisions.
+    const isCircle1 = obj1 instanceof Circle ? true : false;
+    const isCircle2 = obj2 instanceof Circle ? true : false;
+
+    if (isCircle1 && isCircle2) {
+        circleCircleCollision(obj1, obj2);
+    } else if (!isCircle1 && ! isCircle2) {
+        polyPolyCollision();
+    } else if (obj1 instanceof Circle) {
+        circlePolyCollision(obj1, obj2);
+    } else {
+        circlePolyCollision(obj2, obj1);
+    }
+
 }
 
-function getOrth(obj) {
-    const orths = [];
+function circleCircleCollision(obj1, obj2) {
+    const distSqr = (obj1.x - obj2.x)**2 + (obj1.y - obj2.y)**2;
+    // Does <= have to be a float such as 0.0? Check later.
+    return distSqr - (obj1.radius + obj2.radius)**2 <= 0;
+}
+
+function circlePolyCollision(obj1, obj2) {
+    // Deal with polygon-circle.
+    // Vertices for polygon.
+    return null;
+}
+
+function polyPolyCollision(obj1, obj2) {
+    // Polygon-polygon.
+    //const axes1 = getAxes(obj1);
+    //const axes2 = getAxes(obj2);
+    return null;
+}
+
+/**
+ * @desc Get the projectionn axes. These are the vectors normal to the polynomial edges.
+ * @param {Polygon} obj - A Polygon to retrieve the axes of.
+ * @returns A list of vectors that have not be normalized to unit vectors.
+ */
+function getAxes(obj) {
+    const axes = [];
     let prev = obj.vertices[obj.vertices.length - 1];
 
     for (const vert of obj.vertices) {
         // 0 is x component, 1 is y.
         const edge = new vec2.fromValues(vert[0] - prev[0], vert[1] - prev[1])
         // Swap x and y components of edge and negate one.
-        orths.push(new vec2.fromValues(-edge[1], edge[0]));
+        axes.push(new vec2.fromValues(-edge[1], edge[0]));
         prev = vert;
     }
 
-    return orths;
+    return axes;
 }
 
 export default isColliding;
