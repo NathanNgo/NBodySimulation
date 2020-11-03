394807022dd822ac3168ace558675228aa3ed52f
Nathan Ngo <nathanj238@gmail.com>
2020-11-04 01:05:23 +1100

    Started Canvas component hook with physics included

---
+++ src/hooks/useCanvasPhys.js
@@ -0,0 +1,9 @@
+import { useRef, useEffect } from 'react';
+
+// A re-render of the canvas will cause it to start simulating with the new initial states.
+// A re-render of the canvas will cause it to start simulating with the new initial parameters.
+function useCanvasPhys(ctx, initVals) {
+    const canvasRef = useRef(null);
+
+
+}
