a39d951c21d03d6e67b40fd24960f30427fee2e0
Nathan Ngo <nathanj238@gmail.com>
2020-11-04 17:55:47 +1100

    Updated useCanasPhys hook to use physics and draw

--- src/hooks/useCanvasPhys.js
+++ src/hooks/useCanvasPhys.js
@@ -1,9 +1,53 @@
 import { useRef, useEffect } from 'react';
+import { draw } from '../render/render.js';
+import Engine from '../physics/engine.js';
 
 // A re-render of the canvas will cause it to start simulating with the new initial states.
 // A re-render of the canvas will cause it to start simulating with the new initial parameters.
-function useCanvasPhys(ctx, initVals) {
+function useCanvasPhys(vals) {
     const canvasRef = useRef(null);
 
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        const ctx = canvas.getContext('2d');
+        let animId;
+        const physEng = new Engine();
 
+        function step(timestamp) {
+            draw(ctx, vals);
+            /* eslint-disable-next-line */ /* We want to reset on re-render.*/
+            vals = physEng.update(vals);
+            animId = window.requestAnimationFrame(step);
+        }
+
+        if (resize(canvas)) {
+            console.log('Resized canvas');
+        } else {
+            console.log('Did not resize canvas');
+        }
+
+        window.requestAnimationFrame(step);
+
+        return () => {
+            window.cancelAnimationFrame(animId);
+        }
+    });
+
+    return canvasRef;
 }
+
+function resize(canvas, ctx) {
+    const { width, height } = canvas.getBoundingClientRect();
+    const ratio = window.devicePixelRatio;
+
+    if (canvas.width !== width*ratio || canvas.height !== height*ratio) {
+        canvas.width = width*ratio;
+        canvas.height = height*ratio;
+        ctx.scale(ratio, ratio);
+        return true;
+    }
+
+    return false;
+}
+
+export default useCanvasPhys;
