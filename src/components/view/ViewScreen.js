32599e591e67bc7daef2906249b6e133ab8830e6
Nathan Ngo <nathanj238@gmail.com>
2020-11-05 03:52:33 +1100

    Passed onResize prop to children

--- src/components/view/ViewScreen.js
+++ src/components/view/ViewScreen.js
@@ -4,8 +4,7 @@ import CanvasPhys from './CanvasPhys';
 function ViewScreen(props) {
     return (
         <div className='containerViewScreen'>
-            <CanvasPhys vals={props.initVals}>
-            </CanvasPhys>
+            <CanvasPhys vals={props.initVals} onResize={props.onResize} />
         </div>
     );
 }
