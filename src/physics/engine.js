2a209a0e6eae2933f82bd9dc3a5d1fcad8c9d318
Nathan Ngo <nathanj238@gmail.com>
2020-11-05 15:01:28 +1100

    Started work on physics engine to update values

--- src/physics/engine.js
+++ src/physics/engine.js
@@ -2,11 +2,26 @@ import { RigidBody, Circle, Polygon } from './rigid';
 import { isColliding } from './collision';
 
 class Engine {
-    constructor() {
+    /**
+     * @constructs Creates an instance of the Engine.
+     */
+    constructor(bounds, gravity) {
+        this.bounds = bounds;
+        this.gravity = gravity;
     }
 
-    update() {
-
+    /**
+     * Updates an array of RigidBody's.
+     * @param {RigidBody[]} vals - An array of RigidBody's or objects inherited from RigidBody.
+     */
+    update(vals) {
+        vals.forEach((body) => {
+            if (body instanceof Circle) {
+                for (let i = 0; i < body.coordinates.length; i++) {
+                    body.coordinates[i] += body.velocity[i];
+                }
+            }
+        });
     }
 }
 
