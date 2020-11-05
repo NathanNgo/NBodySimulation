3c5dc41fd47295bf84532ec8138ef8fa75f6abc2
Nathan Ngo <nathanj238@gmail.com>
2020-11-05 14:59:22 +1100

    Refactored canvasHasResized and changed heading

--- src/components/NBodySimulation.js
+++ src/components/NBodySimulation.js
@@ -9,16 +9,16 @@ class NBodySimulation extends React.Component {
         super(props);
 
         this.state = {
-            initVals: [new Circle(0, 0, 33, 50, 60), new Circle(0, 0, 77, 344, 455)],
+            initVals: [new Circle(0, [1, 0], 33, [50, 60]), new Circle(0, [1, 2], 77, [344, 455])],
             canvasHasResized: false,
             canvasWidth: undefined,
             canvasHeight: undefined
         }
 
-        this.canvasHasResized = this.canvasHasResized.bind(this);
+        this.handleCanvasResize = this.handleCanvasResize.bind(this);
     }
 
-    canvasHasResized(width, height) {
+    handleCanvasResize(width, height) {
         if (!this.state.canvasHasResized) {
             this.setState({ canvasHasResized: true, canvasWidth: width, canvasHeight: height })
         }
@@ -28,13 +28,14 @@ class NBodySimulation extends React.Component {
         return (
             <div className='containerMain'>
                 <header className='header'>
-                    <h1> <span className='acc'>N Body Simulation (Work In Progress)</span> </h1>
+                    {/*<h1> <span className='acc'>N Body Simulation (Work In Progress)</span> </h1>*/}
+                    <h1> <span className='acc'> Physics Simulation </span> </h1>
                 </header>
                 <section className='sideBar'>
                     {this.state.canvasHasResized ? <SideBar /> : <h2> Loading... </h2>}
                 </section>
                 <section className='viewScreen'>
-                    <ViewScreen initVals={this.state.initVals} onResize={this.canvasHasResized} />
+                    <ViewScreen initVals={this.state.initVals} onResize={this.handleCanvasResize} />
                 </section>
                 <footer className='footer'>
                     <p> <a href='https://github.com/NathanNgo/NBodySimulation'> GitHub </a> </p>
