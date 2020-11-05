fa4d8f284aaff0ebf51ccb8d077326a061a04854
Nathan Ngo <nathanj238@gmail.com>
2020-11-05 15:10:52 +1100

    Renamed component to better represent project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './css/App.css';
-import { NBodySimulation } from './components/NBodySimulation';
+import { PhysicsSimulation } from './components/PhysicsSimulation';
 
 // First component. Used to set up data structures and other objects
 // which might be used by child components. Can be passed via props.
 // e.g. Axios object to communicate to back end (Setting baseURL and other settings).
 function App() {
     return (
-        <NBodySimulation />
+        <PhysicsSimulation />
     );
 }
 
