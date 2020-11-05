b7166acfc3ee34ab7b6dea654bd050a5ec265d81
Nathan Ngo <nathanj238@gmail.com>
2020-11-05 15:02:04 +1100

    Reworked how RigidBody properties are represented

--- src/physics/rigid.js
+++ src/physics/rigid.js
@@ -1,21 +1,20 @@
 class RigidBody {
-    constructor(mass = 1.0, velocity = 0.0) {
+    constructor(mass = 1, velocity = 0) {
         this.mass = mass;
         this.velocity = velocity;
     }
 }
 
 class Circle extends RigidBody {
-    constructor(mass = 1.0, velocity = 0.0, radius = 1.0, x = 0.0, y = 0.0) {
+    constructor(mass = 1, velocity = [0, 0], radius = 1, coordinates = [0, 0]) {
         super(mass, velocity);
         this.radius = radius;
-        this.x = x;
-        this.y = y;
+        this.coordinates = coordinates;
     }
 }
 
 class Polygon extends RigidBody {
-    constructor(mass = 1.0, vertices = [0.0, 0.0]) {
+    constructor(mass = 1, vertices = [0, 0]) {
         super(mass);
         this.vertices = vertices;
     }
