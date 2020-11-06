66167d101e301cec427255da2390a7718e17eae1
Nathan Ngo <nathanj238@gmail.com>
2020-11-07 04:49:54 +1100

    Added rudimentary collision resolution

--- src/physics/collision/collisionResolution.js
+++ src/physics/collision/collisionResolution.js
@@ -1,6 +1,20 @@
+import { vec2 } from 'gl-matrix';
+
 function resolveCollision(obj1, obj2, settings) {
-    // Apply kinematic equations and coefficient of restitution.
-    return
+    // Apply kinematic equations and coefficient of restitution. Below leads to
+    // unnatural collisions for the moment.
+    for (let i = 0; i < obj1.coordinates.length; i++) {
+        const obj1FirstTerm = ((obj1.mass - obj2.mass)/(obj1.mass + obj2.mass))*obj1.velocity[i];
+        const obj1SecondTerm = ((2*obj2.mass)/(obj1.mass + obj2.mass))*obj2.velocity[i];
+        const obj1Vel = obj1FirstTerm + obj1SecondTerm;
+
+        const obj2FirstTerm = ((2*obj1.mass)/(obj1.mass + obj2.mass))*obj1.velocity[i];
+        const obj2SecondTerm = ((obj2.mass - obj1.mass)/(obj1.mass + obj2.mass))*obj2.velocity[i];
+        const obj2Vel = obj2FirstTerm + obj2SecondTerm;
+
+        obj1.velocity[i] = obj1Vel;
+        obj2.velocity[i] = obj2Vel;
+    }
 }
 
 function resolveBoundaryCollision(obj, boundarySide, settings) {
