64c97cf25e7aa72a2680f325cc6a63501efd6e86
Nathan Ngo <nathanj238@gmail.com>
2020-11-06 22:10:03 +1100

    Updated RigidBody and other subclasses to have color

--- src/physics/rigid.js
+++ src/physics/rigid.js
@@ -1,21 +1,22 @@
 class RigidBody {
-    constructor(mass = 1, velocity = 0) {
+    constructor(mass = 1, velocity = 0, color = 'white') {
         this.mass = mass;
         this.velocity = velocity;
+        this.color = color;
     }
 }
 
 class Circle extends RigidBody {
-    constructor(mass = 1, velocity = [0, 0], radius = 1, coordinates = [0, 0]) {
-        super(mass, velocity);
+    constructor(mass = 1, velocity = [0, 0], radius = 1, coordinates = [0, 0], color = 'white') {
+        super(mass, velocity, color);
         this.radius = radius;
         this.coordinates = coordinates;
     }
 }
 
 class Polygon extends RigidBody {
-    constructor(mass = 1, vertices = [0, 0]) {
-        super(mass);
+    constructor(mass = 1, velocity = [0, 0], vertices = [[0, 0]], color = 'white') {
+        super(mass, velocity, color);
         this.vertices = vertices;
     }
 }
