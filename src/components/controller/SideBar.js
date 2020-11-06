86df7a9d3816dac2dad646e4a5dbb6fa96c417f4
Nathan Ngo <nathanj238@gmail.com>
2020-11-07 04:48:07 +1100

    Fixed style and added warning

--- src/components/controller/SideBar.js
+++ src/components/controller/SideBar.js
@@ -7,32 +7,38 @@ import resetIcon from '../../css/icons/reset-24px.svg';
 
 function SideBar() {
     return (
-      <div>
-          <ClosablePanel title='Information'>
-              {/*<p> The <b>N Body Simulation</b> is designed to represent collisions and interactions between <span className='acc'>N</span> objects. </p>*/}
-              <p> A simple demonstration of a lightweight physics engine. Still a <span className='acc'> work in progress </span> and very much incomplete. </p>
-          </ClosablePanel>
-          <ClosablePanel title='Controls'>
-              <Button
-                  className='playButton'
-                  iconURL={playIcon}
-                  //onClick=...
-              />
-              <Button
-                  className='pauseButton'
-                  iconURL={pauseIcon}
-                  //onClick=...
-              />
-              <Button
-                  className='resetButton'
-                  iconURL={resetIcon}
-                  //onClick=...
-              />
-          </ClosablePanel>
-          <ClosablePanel title='Options'>
-              <p> Options go here </p>
-          </ClosablePanel>
-      </div>
+        <div>
+            <ClosablePanel title='Information'>
+                {/*<p> The <b>N Body Simulation</b> is designed to represent collisions and interactions between <span className='acc'>N</span> objects. </p>*/}
+                <h3 style={{marginBottom: '10px'}}> To obtain a new sample, refresh the page. </h3>
+                <p>
+                    A simple demonstration of a lightweight physics engine. Still a
+                    <span className='acc'> work in progress </span> and very much incomplete.
+                    Currently, collision resolution is <span className='acc'>not working as intended </span>
+                    and I intend to fix this later.
+                </p>
+            </ClosablePanel>
+            <ClosablePanel title='Controls'>
+                <Button
+                    className='playButton'
+                    iconURL={playIcon}
+                    //onClick=...
+                />
+                <Button
+                    className='pauseButton'
+                    iconURL={pauseIcon}
+                    //onClick=...
+                />
+                <Button
+                    className='resetButton'
+                    iconURL={resetIcon}
+                    //onClick=...
+                />
+            </ClosablePanel>
+            <ClosablePanel title='Options'>
+                <p> Options go here </p>
+            </ClosablePanel>
+        </div>
     );
 }
 
