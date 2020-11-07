37b77df5dc187c8cc942c005f8937d81aad1bc48
Nathan Ngo <nathanj238@gmail.com>
2020-11-08 00:17:46 +1100

    Collision detection now accounts for location of contact

--- src/physics/collision/collisionResolution.js
+++ src/physics/collision/collisionResolution.js
@@ -1,20 +1,37 @@
-import { vec2 } from 'gl-matrix';
+import { vec2 as v } from 'gl-matrix';
 
 function resolveCollision(obj1, obj2, settings) {
-    // Apply kinematic equations and coefficient of restitution. Below leads to
-    // unnatural collisions for the moment.
-    for (let i = 0; i < obj1.coordinates.length; i++) {
-        const obj1FirstTerm = ((obj1.mass - obj2.mass)/(obj1.mass + obj2.mass))*obj1.velocity[i];
-        const obj1SecondTerm = ((2*obj2.mass)/(obj1.mass + obj2.mass))*obj2.velocity[i];
-        const obj1Vel = obj1FirstTerm + obj1SecondTerm;
+    // TODO: Move circles away from each other first, so they don't stick. Find MPV.
 
-        const obj2FirstTerm = ((2*obj1.mass)/(obj1.mass + obj2.mass))*obj1.velocity[i];
-        const obj2SecondTerm = ((obj2.mass - obj1.mass)/(obj1.mass + obj2.mass))*obj2.velocity[i];
-        const obj2Vel = obj2FirstTerm + obj2SecondTerm;
+    /* Create the unit normal vector. */
+    const components = [
+        obj1.coordinates[0] - obj2.coordinates[0],
+        obj1.coordinates[1] - obj2.coordinates[1]
+    ];
+    const unitNorm = v.normalize([], v.fromValues(...components));
 
-        obj1.velocity[i] = obj1Vel;
-        obj2.velocity[i] = obj2Vel;
-    }
+    /* Convert veolcity array into vector object. */
+    const obj1Vel = v.fromValues(obj1.velocity[0], obj1.velocity[1]);
+    const obj2Vel = v.fromValues(obj2.velocity[0], obj2.velocity[1]);
+
+    /* Obtain parallel and perpendicular projection vectors onto the unitNorm vector. */
+    const obj1InitPar = v.scale([], unitNorm, v.dot(obj1Vel, unitNorm));
+    const obj2InitPar = v.scale([], unitNorm, v.dot(obj2Vel, unitNorm));
+    const obj1InitPerp = v.sub([], obj1Vel, obj1InitPar);
+    const obj2InitPerp = v.sub([], obj2Vel, obj2InitPar);
+
+    /* Find the final parallel vectors. */
+    const obj1FirstCoeff = (obj1.mass - obj2.mass)/(obj1.mass + obj2.mass);
+    const obj1SecondCoeff = (2*obj2.mass)/(obj1.mass + obj2.mass);
+    const obj1Fin = v.add([], v.scale([], obj1InitPar, obj1FirstCoeff), v.scale([], obj2InitPar, obj1SecondCoeff));
+
+    const obj2FirstCoeff = (obj2.mass - obj1.mass)/(obj1.mass + obj2.mass);
+    const obj2SecondCoeff = (2*obj1.mass)/(obj1.mass + obj2.mass);
+    const obj2Fin = v.add([], v.scale([], obj2InitPar, obj2FirstCoeff), v.scale([], obj1InitPar, obj2SecondCoeff));
+
+    /* Change the velocity. */
+    obj1.velocity = v.add([], obj1Fin, obj1InitPerp);
+    obj2.velocity = v.add([], obj2Fin, obj2InitPerp);
 }
 
 function resolveBoundaryCollision(obj, boundarySide, settings) {
