1ff3d764b273fdbe8a249b6ac5619a8b1fd52550
Nathan Ngo <nathanj238@gmail.com>
2020-11-11 02:28:30 +1100

    Reworked RigidBody and subclass to have Object parameter

--- src/physics/rigid.js
+++ src/physics/rigid.js
@@ -1,30 +1,43 @@
 class RigidBody {
-    constructor(mass, velocity, color) {
+    constructor(attributes) {
+        const { mass, velocity, color, active, cor } = attributes;
+
+        if (cor < 0 || cor > 1) {
+            throw new RangeError('COR value must be between 0 and 1.');
+        }
+
         this.mass = mass;
         this.velocity = velocity;
         this.color = color;
+        this.active = active;
+        this.cor = cor;
     }
 }
 
 class Circle extends RigidBody {
     /**
      * @construct Creates a Circle object.
-     * @param {number} mass - The mass of the circle.
-     * @param {number[]} velocity - The velocity of the circle.
-     * @param {number} radius - The radius of the circle.
-     * @param {numer[]} coords - The position of the circle.
-     * @param {String} color - A hex string or value to represent color.
+     * @property {number} mass - The mass of the circle.
+     * @property {number[]} velocity - The velocity of the circle.
+     * @property {number} radius - The radius of the circle.
+     * @property {number[]} coords - The position of the circle.
+     * @property {String} color - A hex string or value to represent color.
+     * @property {boolean} active - If the object is sleeping or not.
+     * @property {number} cor - Coefficient of Restitution. Number between 0 and 1.
      */
-    constructor(mass, velocity, radius, coords, color) {
-        super(mass, velocity, color);
+    constructor(attributes) {
+        const { radius, coords, ...rest } = attributes;
+
+        super(rest);
         this.radius = radius;
         this.coords = coords;
     }
 }
 
 class Polygon extends RigidBody {
-    constructor(mass, velocity, vertices, color) {
-        super(mass, velocity, color);
+    constructor(attributes) {
+        const { vertices, ...rest } = attributes;
+        super(rest);
         this.vertices = vertices;
     }
 }
