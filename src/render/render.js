c43db55b18875d070d13c1c1c57f2eadad5f6c46
Nathan Ngo <nathanj238@gmail.com>
2020-11-13 16:50:37 +1100

    Added JSdoc to rigid.js

--- src/render/render.js
+++ src/render/render.js
@@ -1,5 +1,10 @@
 import { Circle } from '../physics/rigid';
 
+/**
+ * Determines the type of an object and draws it to a Canvas element.
+ * @params {Object} ctx - The context object for the Canvas element.
+ * @params {RigidBody[]} vals - An array of RigidBody objects.
+ */
 function draw(ctx, vals) {
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
@@ -10,6 +15,11 @@ function draw(ctx, vals) {
     }
 }
 
+/**
+ * Draws a Circle object onto the canvas element.
+ * @params {Object} ctx - The context object for the canvas element.
+ * @params {Circle} circle - The Circle object we want to draw.
+ */
 function drawCircle(ctx, circle) {
     const [ x, y ] = circle.coords;
     ctx.beginPath();
