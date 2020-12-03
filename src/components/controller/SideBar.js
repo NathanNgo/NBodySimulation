c06146666a9ea1a748ffbec5ae452dad49f18e41
Nathan Ngo <nathanj238@gmail.com>
2020-12-03 21:37:11 +1100

    Fixed spelling mistake

--- src/components/controller/SideBar.js
+++ src/components/controller/SideBar.js
@@ -17,7 +17,7 @@ function SideBar(props) {
                     Here's an explanation of the engine's options: <br /> <br />
                     <b> Number of Objects: </b> The exact number of objects to generate. <br /><br />
                     <b> Coeff. of Restitution: </b> The amount of energy which remains after a collision. A value of 1 is perfectly
-                    elastic. A value of 0 is perflectly inelastic. Other values can also be explored.<br /><br />
+                    elastic. A value of 0 is perfectly inelastic. Other values can also be explored.<br /><br />
                     <b> Gravity (Vertical): </b> The amount of gravity in the vertical direction. Values between 0 and 0.5 provide
                     the best results, but other values can be explored.<br /><br />
                     <b> Gravity (Horrizontal): </b> The amount of gravity in the horrizontal direction. Values between 0 and 0.5 provide
