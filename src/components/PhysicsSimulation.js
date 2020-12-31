a7de185f840d95fc6cc9c8b862fdc23ccba4c07d
Nathan Ngo <nathanj238@gmail.com>
2020-12-31 18:46:25 +1100

    Seperated settings properties and reworked value generation

--- src/components/PhysicsSimulation.js
+++ src/components/PhysicsSimulation.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { SideBar } from './controller/SideBar';
 import { ViewScreen } from './view/ViewScreen';
+import { genColorGradient, genRandAttr, genCircleVals } from '../utils/generators';
 import { Circle } from '../physics/rigid';
 import Modal from './controller/containers/Modal';
 import FPSStats from 'react-fps-stats';
@@ -12,13 +13,19 @@ class PhysicsSimulation extends React.Component {
 
         this.state = {
             vals: [],
-            // Settings is maintained a 2 different values instead of a single vector to avoid
-            // the need to deep clone the settings objects.
-            settings: {
-                amount: 0,
-                gravX: 0.00,
-                gravY: 0.00,
+            engineSettings: {
+                gravity: [0.0, 0.0],
                 cor: 1.00,
+            },
+            genSettings: {
+                amount: 0,
+                minRadius: 0,
+                maxRadius: 0,
+                minVel: 0,
+                maxVel: 0,
+                minMass: 0,
+                maxMass: 0,
+                massGradient: false,
             },
             canvasWidth: undefined,
             canvasHeight: undefined,
@@ -30,65 +37,31 @@ class PhysicsSimulation extends React.Component {
 
         this.handleCanvasRender = this.handleCanvasRender.bind(this);
         this.handleAnimToggle = this.handleAnimToggle.bind(this);
-        this.handleSettingsSubmit = this.handleSettingsSubmit.bind(this);
+        this.handleEngSettingsSubmit = this.handleEngSettingsSubmit.bind(this);
+        this.handleGenSettingsSubmit = this.handleGenSettingsSubmit.bind(this);
         this.handleModalClose = this.handleModalClose.bind(this);
         this.handleModalOpen = this.handleModalOpen.bind(this);
     }
 
-    /**
-     * Generates random attributes for a Circle Object.
-     * @params {number} width - The max width of the Canvas.
-     * @params {number} height - The max height of the Canvas.
-     * @returns {Object} An object containing all the random attributes for a Circle.
-     */
-    _genRandAttr(width, height) {
-        const mass = Math.random()*(5 - 1) + 1;
-        const radius = Math.random()*(60 - 10) + 10;
-        const xVel = Math.random()*3;
-        const xSign = Math.random() < 0.5 ? -1 : 1;
-        const yVel = Math.random()*3;
-        const ySign = Math.random() < 0.5 ? -1 : 1;
-        const xPos = Math.random()*(width - 2*radius) + radius;
-        const yPos = Math.random()*(height - 2*radius) + radius;
-        const color = '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);
-
-        return {
-            mass,
-            velocity: [xSign*xVel, ySign*yVel],
-            color,
-            active: true,
-            cor: 1,
-            radius,
-            coords: [xPos, yPos]
-        }
-    }
-
-    /**
-     * Generates an array of random Circle objects.
-     * @params {number} width - The max width of the Canvas.
-     * @params {number} height - The max height of the Canvas.
-     * @params {number} amount - The number of Circle objects to generate.
-     * @returns {Circle[]} An array containing all randomly generated Circle objects.
-     */
-    _genVals(width, height, amount) {
-        const vals = []
-        for (let i = 0; i < amount; i++) {
-            vals.push(new Circle(this._genRandAttr(width, height)));
-        }
-
-        return vals;
-    }
-
     handleCanvasRender(width, height) {
         if (!this.state.canvasHasRendered) {
-            const amount = Math.floor(Math.random()*(20 - 8) + 8);
+            const initGenSettings = {
+                amount: Math.floor(Math.random()*(20 - 10) + 10),
+                minRadius: 10,
+                maxRadius: 60,
+                minVel: 0,
+                maxVel: 3,
+                minMass: 1,
+                maxMass: 5,
+                massGradient: false
+            };
 
             this.setState((state) => ({
                 canvasHasRendered: true,
                 canvasWidth: width,
                 canvasHeight: height,
-                vals: this._genVals(width, height, amount),
-                settings: { ...state.settings, amount }
+                vals: genCircleVals(width, height, initGenSettings),
+                genSettings: initGenSettings
             }));
         }
     }
@@ -97,12 +70,14 @@ class PhysicsSimulation extends React.Component {
         this.setState({ animIsPlaying });
     }
 
-    handleSettingsSubmit(settings) {
-        // this.setState({ vals, settings });
-        /* Just have it generate new values for now. Implement altering the existing values later */
+    handleEngSettingsSubmit(engineSettings) {
+        this.setState({ engineSettings });
+    }
+
+    handleGenSettingsSubmit(genSettings) {
         const { canvasWidth, canvasHeight } = this.state;
-        const newVals = this._genVals(canvasWidth, canvasHeight, settings.amount);
-        this.setState({ vals: newVals, settings });
+        const vals = genCircleVals(canvasWidth, canvasHeight, genSettings);
+        this.setState({ genSettings, vals });
     }
 
     handleModalClose() {
@@ -115,16 +90,19 @@ class PhysicsSimulation extends React.Component {
 
     render() {
         let sideBar;
+
         if (this.state.canvasHasRendered) {
             sideBar =
                 <SideBar
                     vals={this.state.vals}
                     canvasWidth={this.state.canvasWidth}
                     canvasHeight={this.state.canvasHeight}
-                    settings={this.state.settings}
+                    engineSettings={this.state.engineSettings}
+                    genSettings={this.state.genSettings}
                     animIsPlaying={this.state.animIsPlaying}
                     onAnimToggle={this.handleAnimToggle}
-                    onSettingsSubmit={this.handleSettingsSubmit}
+                    onEngSettingsSubmit={this.handleEngSettingsSubmit}
+                    onGenSettingsSubmit={this.handleGenSettingsSubmit}
                 />;
         } else {
             sideBar = <h2> Loading... </h2>;
@@ -144,7 +122,7 @@ class PhysicsSimulation extends React.Component {
                     <ViewScreen
                         vals={this.state.vals}
                         onRender={this.handleCanvasRender}
-                        settings={this.state.settings}
+                        engineSettings={this.state.engineSettings}
                     />
                 </section>
                 <footer className='footer'>
