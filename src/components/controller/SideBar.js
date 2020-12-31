c022f385fa2c78ae642b0db7632eded13169a85a
Nathan Ngo <nathanj238@gmail.com>
2020-12-31 18:46:57 +1100

    Cleaned up Information Panels and Sidebar

--- src/components/controller/SideBar.js
+++ src/components/controller/SideBar.js
@@ -1,36 +1,49 @@
 import React from 'react';
-import ClosablePanel from './containers/ClosablePanel';
-import SettingsList from './input/SettingsList';
+import Panel from './containers/Panel';
+import EngineSettingsList from './input/EngineSettingsList';
+import GenSettingsList from './input/GenSettingsList';
 import TabContainer from './containers/TabContainer';
-import Tab from './containers/Tab';
-/*
-import { Button } from './input/Button';
-import playIcon from '../../css/icons/play_arrow-24px.svg';
-import pauseIcon from '../../css/icons/pause-24px.svg';
-import resetIcon from '../../css/icons/reset-24px.svg';
-*/
 
 function SideBar(props) {
     return (
         <div>
             <TabContainer>
                 <div label='Information'>
-                    <p>
-                        This is a demonstration of a lightweight physics engine. You can adjust the engine's parameters in the
-                        <span className='acc'> options</span> tab. <br /><br />
-
-                        Here's an explanation of the engine's options: <br /> <br />
-                        <b> Number of Objects: </b> The exact number of objects to generate. <br /><br />
-                        <b> Coeff. of Restitution: </b> The amount of energy which remains after a collision. A value of 1 is perfectly
-                        elastic. A value of 0 is perfectly inelastic. Other values can also be explored.<br /><br />
-                        <b> Gravity (Vertical): </b> The amount of gravity in the vertical direction. Values between 0 and 0.5 provide
-                        the best results, but other values can be explored.<br /><br />
-                        <b> Gravity (Horizontal): </b> The amount of gravity in the horizontal direction. Values between 0 and 0.5 provide
-                        the best results, but other values can be explored.
-                    </p>
+                    <Panel title='Introduction'>
+                        <p>
+                            This is a demonstration of a lightweight physics engine. You can adjust the engine's parameters in the
+                            <span className='acc'> options</span> tab. <br /><br />
+                        </p>
+                    </Panel>
+                    <Panel title='Generation Parameters'>
+                        <p>
+                            <b><u>Number of Objects</u></b>: The exact number of objects to generate.<br /><br />
+                            <b><u>Radius (Min)</u></b>: The minimum radius an object can be generated with.<br /><br />
+                            <b><u>Radius (Max)</u></b>: The maximum radius an object can be generated with.<br /><br />
+                            <b><u>Velocity (Min)</u></b>: The minimum velocity an object can be generated with.<br /><br />
+                            <b><u>Velocity (Max)</u></b>: The maximum velocity an object can be generated with.<br /><br />
+                            <b><u>Mass (Min)</u></b>: The minimum mass an object can be generated with.<br /><br />
+                            <b><u>Mass (Max)</u></b>: The maximum mass an object can be generated with.<br /><br />
+                        </p>
+                    </Panel>
+                    <Panel title='Engine Parameters'>
+                        <p>
+                            <b><u>Coeff. of Restitution</u></b>: The amount of energy which remains after a collision. A value of 1 is perfectly
+                            elastic. A value of 0 is perfectly inelastic. Other values can also be explored.<br /><br />
+                            <b><u>Gravity (Vertical)</u></b>: The amount of gravity in the vertical direction. Values between 0 and 0.5 provide
+                            the best results, but other values can be explored.<br /><br />
+                            <b><u>Gravity (Horizontal)</u></b>: The amount of gravity in the horizontal direction. Values between 0 and 0.5 provide
+                            the best results, but other values can be explored.
+                        </p>
+                    </Panel>
                 </div>
                 <div label='Options'>
-                    <SettingsList settings={props.settings} onSettingsSubmit={props.onSettingsSubmit} />
+                    <Panel title="Generation Settings">
+                        <GenSettingsList settings={props.genSettings} onGenSettingsSubmit={props.onGenSettingsSubmit} />
+                    </Panel>
+                    <Panel title='Engine Settings'>
+                        <EngineSettingsList settings={props.engineSettings} onEngSettingsSubmit={props.onEngSettingsSubmit} />
+                    </Panel>
                 </div>
             </TabContainer>
         </div>
